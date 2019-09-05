c0f41d9d3831478e7d7add94904418cf29dbf807
victorbohmer <37998629+victorbohmer@users.noreply.github.com>
2019-09-05 11:15:26 +0200

    Classes added

---
+++ StarWars2/Models/Star.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars2.Models
+{
+    public class Star
+    {
+        public int Id { get; set; }
+        public int StarSystemId { get; set; }
+        public StarSystem StarSystem { get; set; }
+        public int StarTypeId { get; set; }
+        public StarType StarType { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ StarWars2/Models/StarSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars2.Models
+{
+    public class StarSystem
+    {
+        public int Id { get; set; }
+        public List<Star> Stars { get; set; }
+        public string Name { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+    }
+}
